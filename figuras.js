be20cc56da2fdc75ba137c64c0d0bfd3a168048e
Daniel Rodriguez <rodriguezdaniel048@gmail.com>
2022-01-13 22:03:32 -0500

    interactuando con HTML

--- figuras.js
+++ figuras.js
@@ -84,3 +84,33 @@ function areaCirculo(radio) {
 // console.log("El area del circulo es: " + areaCirculo + "cm2");
 
 console.groupEnd();
+
+// Interactuar con HTML
+
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
+
+function calcularPerimetroTriangulo() {
+  const input1 = document.getElementById("inputTriangulo1");
+  const input2 = document.getElementById("inputTriangulo2");
+  const input3 = document.getElementById("inputBaseTriangulo");
+  const value1 = parseInt(input1.value);
+  const value2 = parseInt(input2.value);
+  const value3 = parseInt(input3.value);
+  const perTrian = perimetroTriangulo(value1, value2, value3);
+
+  alert(perTrian);
+}
